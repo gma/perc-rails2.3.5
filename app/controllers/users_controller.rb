8151135e7c71f5ac6452bdf8288b5b9b59605561
Edward Cervenka <ed@Edward-Cervenkas-MacBook-Pro.local>
2011-04-29 16:58:25 +0100

    Simple User signup complete. Removed unnecessary css files. Need to fix error css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,17 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Percussion Orchestrations Registration"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @title = "Percussion Orchestrations Registration"
+      render 'new'
+    end
+  end      
 end
