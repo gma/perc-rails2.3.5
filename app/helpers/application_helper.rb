88dba0d776317062bab2ed74fa3d15d0dcfa958b
Graham Ashton <graham@effectif.com>
2011-05-15 18:45:52 +0100

    Mark current page as active.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def nav_link_to(text, path)
+    html_classes = if (path == request.path) 
+      { :class => 'active' }
+    else
+      {}
+    end
+    link_to text, path, html_classes
+  end
 end
