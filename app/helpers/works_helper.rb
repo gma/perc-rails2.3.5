091fe4b7c504a294087324bee1487a2f34df2433
Graham Ashton <graham@effectif.com>
2011-05-15 14:22:46 +0100

    Moved works table cells into helpers.

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,2 +1,16 @@
 module WorksHelper
+  def render_table_header(instrument, number = nil)
+    method = number.nil? ? instrument : "#{instrument}_#{number}?"
+    if @workdetail.send(method)
+      "<th>#{instrument.capitalize} #{number || 1}</th>".html_safe
+    end
+  end
+
+  def render_table_cell(workdetail, instrument, number = nil)
+    method = number.nil? ? instrument : "#{instrument}_#{number}"
+    data = workdetail.send(method)
+    if data.present?
+      "<td>#{data}</td>".html_safe
+    end
+  end
 end
